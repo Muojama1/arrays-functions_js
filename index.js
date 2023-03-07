ba9630ace8037d238ff6c83291c7c8a950a731e3
Muojama1 <sandramuojama@gmail.com>
2023-03-07 16:37:55 +0100

    files

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// QUESTION No. 1
+
+
 // QUESTION NO. 2
 // 1. ADD 'Kotlin' to the end of the array
 let languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python']
@@ -19,10 +22,21 @@ let languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python']
 languages.splice (0, 0, 'Scala', 'Swift')
 console.log (languages)
 
-5. Replace 'PHP' with 'Go' and 'Rust'
+// 5. Replace 'PHP' with 'Go' and 'Rust'
 let languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python']
 languages.splice (3, 1, 'Go', 'Rust')
 console.log (languages)
+
+
+// QUESTION No. 3
+let fruit = ['apple', 'mango', 'banana',];
+function changeFruit(fruit) {
+    fruit[2] = 'orange';
+    return fruit;
+}
+console.log(changeFruit(fruit))
+
+value of fruit = [ 'apple', 'mango', 'orange' ]
 
 
 
